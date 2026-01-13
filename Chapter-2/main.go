d094e0ed9d4432b18f0d9c5f5a285cfd1974dd85
exy <teslawiktor@gmail.com>
2026-01-13 21:20:38 +0100

    38:40

---
+++ Chapter-2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//-------------------------------------------------------------------------------------------
+	//Declaring a variable
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+
+	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username,)
+	//-------------------------------------------------------------------------------------------
+	//Short variable declaration
+	congrats := "happy birthday!"
+
+	fmt.Println(congrats)
+	//-------------------------------------------------------------------------------------------
+	//Same line declaration
+	averageOpenRate, displayMessage := .23, "is the average open rate of your message"
+
+	fmt.Println(averageOpenRate, displayMessage)
+	//-------------------------------------------------------------------------------------------
+	accountAge := 2.6
+
+	accountAgeInt := int(accountAge)
+
+	fmt.Println("Your account has existed for", accountAgeInt, "years")
+}
