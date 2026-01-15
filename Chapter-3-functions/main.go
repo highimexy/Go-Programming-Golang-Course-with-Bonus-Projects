5f71f6dc3a93a5c4c28c19c85af903919dd8906d
exy <teslawiktor@gmail.com>
2026-01-15 19:03:19 +0100

    Chapter 3 - Functions

---
+++ Chapter-3-functions/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//----------------------------------------------------------------
+//Functions
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
+
+//----------------------------------------------------------------
+//Multiple Parameters
+//func concat(s1, s2 string) string {
+//	return s1 + s2
+// }
+
+func main(){
+	fmt.Println(concat("Lane,", " happy birthday!"))
+	fmt.Println(concat("Elon,", " hope that Tesla thing works out"))
+	fmt.Println(concat("Go", " is fantastic"))
+
+}
+    
